31290074a46e51e355f25ed5beab04f7fea342bc
Kliton <barekliton@gmail.com>
2023-12-17 20:36:01 +0100

    added nvim

---
+++ .config/nvim/lua/kliton/packer.lua
@@ -0,0 +1,34 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use ({ 'projekt0n/github-nvim-theme' })
+
+  use {
+	  'nvim-treesitter/nvim-treesitter',
+	  run = ':TSUpdate'
+  }
+
+  use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
+  use {
+	  "ThePrimeagen/harpoon",
+	  branch = "harpoon2",
+	  requires = { {"nvim-lua/plenary.nvim"} }
+  }
+
+  use 'mbbill/undotree'
+  use 'tpope/vim-fugitive'
+end)
+
