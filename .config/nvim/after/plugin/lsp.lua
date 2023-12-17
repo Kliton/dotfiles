3de3923c5c3e3161082a892adaf0263eed9bcdbf
Kliton <barekliton@gmail.com>
2023-12-17 21:13:27 +0100

    fix

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,30 +1,30 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {"rust_analyzer", "tsserver","lua_ls","sqls", "gopls", "jsonls",},
-handlers = {
-    lsp_zero.default_setup,
-  },
+	ensure_installed = { "rust_analyzer", "tsserver", "lua_ls", "gopls", "jsonls", },
+	handlers = {
+		lsp_zero.default_setup,
+	},
 })
 
 lsp_zero.set_sign_icons({
-  error = "✘",
-  warn = "▲",
-  hint = "⚑",
-  info = "»",
+	error = "✘",
+	warn = "▲",
+	hint = "⚑",
+	info = "»",
 })
 
 lsp_zero.format_on_save({
-  servers = {
-    ["null-ls"] = { "javascript", "typescript", "lua" },
-    ["lua_ls"] = { "lua" },
-    ["tsserver"] = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
-  },
+	servers = {
+		["null-ls"] = { "javascript", "typescript", "lua" },
+		["lua_ls"] = { "lua" },
+		["tsserver"] = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
+	},
 })
