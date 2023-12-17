c5329e66dee74abfa09908e38169b751aba39022
Kliton <barekliton@gmail.com>
2023-12-17 16:23:18 +0100

    init repo

--- install.sh
+++ install.sh
@@ -2,15 +2,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-DOT_FOLDERS="zsh,yabai"
-
-for folder in $(echo $DOT_FOLDERS | sed "s/,/ /g"); do
-    echo "[+] Folder :: $folder"
-    stow --ignore=README.md --ignore=LICENSE \
-        -t $HOME -D $folder
-    stow -v -t $HOME $folder
-done
-
+stow .
 # Reload shell once installed
 echo "[+] Reloading shell..."
 exec $SHELL -l
